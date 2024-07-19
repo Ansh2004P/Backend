c16935949b2808e1b9126edceb216dd75c7ab3c5
Ansh2004P <anshkpatel15@gmail.com>
2024-07-20 03:20:45 +0530

    Update Prettier configuration and add middleware for user routes

--- src/app.js
+++ src/app.js
@@ -11,4 +11,14 @@ app.use(
     })
 )
 
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+
+//routes import
+import userRoutes from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users", userRoutes)
+
 export { app }
